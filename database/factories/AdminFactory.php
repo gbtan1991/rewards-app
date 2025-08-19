3368e0a72b3e0051f4c3cc6632eaaa0f2d227259
gbtan23 <gilbert.tan1991@gmail.com>
2025-08-19 20:40:15 +0800

    Created adminfactory for testing

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,22 +2,26 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Admin;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
  */
 class AdminFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Admin::class;
+
     public function definition(): array
     {
         return [
-            //
+            'username' => $this->faker->unique()->userName,
+            'password' => Hash::make('password123'), // default password
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'admin_type' => 'super_admin',
+            'account_status' => 'active', // default
         ];
     }
 }
